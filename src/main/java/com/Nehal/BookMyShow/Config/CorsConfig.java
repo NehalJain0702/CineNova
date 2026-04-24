a5fc39eefae9be070662c38b09e7626c14f1b1a6
Nehal <07nehaljain@gmail.com>
2026-04-24 20:47:27 +0530

    Fix CORS for production

--- src/main/java/com/Nehal/BookMyShow/Config/CorsConfig.java
+++ src/main/java/com/Nehal/BookMyShow/Config/CorsConfig.java
@@ -3,20 +3,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+    @Configuration public class CorsConfig {
 
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+        @Bean public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("*") .
+                            allowedHeaders("*");
+                }
+            };
+        }
     }
