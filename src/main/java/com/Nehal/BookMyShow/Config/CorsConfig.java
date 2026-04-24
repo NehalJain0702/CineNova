c3947dad01def1dde505831d129e19e40063ec76
Nehal <07nehaljain@gmail.com>
2026-04-24 20:29:05 +0530

    Fix CORS for production

--- src/main/java/com/Nehal/BookMyShow/Config/CorsConfig.java
+++ src/main/java/com/Nehal/BookMyShow/Config/CorsConfig.java
@@ -10,17 +10,13 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://localhost:5173"
-                        )
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
     }
-}
+    }
